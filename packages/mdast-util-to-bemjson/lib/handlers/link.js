1196b767275fc40bf531231cb32a10fadc3e9587
Vladimir Grinenko <i@tadatuta.com>
2019-06-20 23:10:38 +0300

    build(npm): Remove normalize-uri package from mdast-util-to-bemjson

--- packages/mdast-util-to-bemjson/lib/handlers/link.js
+++ packages/mdast-util-to-bemjson/lib/handlers/link.js
@@ -1,13 +1,11 @@
 'use strict';
 
-const normalize = require('normalize-uri');
-
 const parseBlock = require('../build-node').parse;
 
 function link(transform, node) {
     const block = parseBlock(node.type);
 
-    const props = { href: normalize(node.url) };
+    const props = { href: encodeURI(decodeURI(node.url)) };
     node.title && (props.title = node.title);
 
     return transform(node, block, props, node.value);
